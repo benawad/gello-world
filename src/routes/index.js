409768bd43e2b15705da68e07d73f7588a452a0e
ben awad <benawad97@gmail.com>
2017-07-08 10:41:39 -0500

    redirect user if they are not authenticated

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,18 +3,52 @@ import {
   BrowserRouter,
   Route,
   Switch,
+  Redirect
 } from 'react-router-dom'
+import decode from 'jwt-decode';
 
 import Auth from './Auth';
 import Login from './Login';
 import Register from './Register';
+
+const checkAuth = () => {
+  const token = localStorage.getItem('token');
+  const refreshToken = localStorage.getItem('refreshToken');
+  if (!token || !refreshToken) {
+    return false;
+  }
+
+  try {
+    // { exp: 12903819203 }
+    const { exp } = decode(refreshToken);
+
+    if (exp < new Date().getTime() / 1000) {
+      return false;
+    }
+
+  } catch (e) {
+    return false;
+  }
+
+  return true;
+}
+
+const AuthRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={props => (
+    checkAuth() ? (
+      <Component {...props} />
+    ) : (
+        <Redirect to={{ pathname: '/login' }} />
+      )
+  )} />
+)
 
 export default () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/login" render={props => <Login {...props} />} />
       <Route exact path="/register" render={props => <Register {...props} />} />
-      <Route exact path="/auth" render={props => <Auth {...props} />} />
+      <AuthRoute exact path="/auth" component={Auth} />
     </Switch>
   </BrowserRouter>
 );
