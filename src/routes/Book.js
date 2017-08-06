6acb3ab710b8d8bc9d4472b7934e9e752922bcf6
ben awad <benawad97@gmail.com>
2017-08-05 19:00:32 -0500

    add dynamic book page

---
+++ src/routes/Book.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { graphql, gql } from "react-apollo";
+import { Redirect } from "react-router-dom";
+
+const Book = ({ data }) => {
+  if (!data) {
+    return <Redirect to={{ pathname: "/404" }} />;
+  }
+
+  const { loading, getBook } = data;
+
+  if (loading) {
+    return <h1>loading...</h1>;
+  }
+
+  if (!loading && !getBook) {
+    return <Redirect to={{ pathname: "/404" }} />;
+  }
+
+  return (
+    <h1>
+      {getBook.title}
+    </h1>
+  );
+};
+
+const bookQuery = gql`
+  query($id: Int!) {
+    getBook(id: $id) {
+      id
+      title
+    }
+  }
+`;
+
+export default graphql(bookQuery, {
+  skip: props => !parseInt(props.match.params.id),
+  options: props => ({
+    variables: { id: props.match.params.id }
+  })
+})(Book);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import {
-  BrowserRouter,
-  Route,
-  Switch,
-  Redirect
-} from 'react-router-dom'
-import decode from 'jwt-decode';
+import React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import decode from "jwt-decode";
 
-import Auth from './Auth';
-import Login from './Login';
-import Register from './Register';
+import Auth from "./Auth";
+import Login from "./Login";
+import Register from "./Register";
+import Book from "./Book";
+import PageNotFound from "./PageNotFound";
 
 const checkAuth = () => {
-  const token = localStorage.getItem('token');
-  const refreshToken = localStorage.getItem('refreshToken');
+  const token = localStorage.getItem("token");
+  const refreshToken = localStorage.getItem("refreshToken");
   if (!token || !refreshToken) {
     return false;
   }
@@ -25,30 +22,29 @@ const checkAuth = () => {
     if (exp < new Date().getTime() / 1000) {
       return false;
     }
-
   } catch (e) {
     return false;
   }
 
   return true;
-}
+};
 
-const AuthRoute = ({ component: Component, ...rest }) => (
-  <Route {...rest} render={props => (
-    checkAuth() ? (
-      <Component {...props} />
-    ) : (
-        <Redirect to={{ pathname: '/login' }} />
-      )
-  )} />
-)
+const AuthRoute = ({ component: Component, ...rest }) =>
+  <Route
+    {...rest}
+    render={props =>
+      checkAuth()
+        ? <Component {...props} />
+        : <Redirect to={{ pathname: "/login" }} />}
+  />;
 
-export default () => (
+export default () =>
   <BrowserRouter>
     <Switch>
       <Route exact path="/login" render={props => <Login {...props} />} />
       <Route exact path="/register" render={props => <Register {...props} />} />
+      <Route exact path="/book/:id" render={props => <Book {...props} />} />
+      <Route exact path="/404" component={PageNotFound} />
       <AuthRoute exact path="/auth" component={Auth} />
     </Switch>
-  </BrowserRouter>
-);
+  </BrowserRouter>;
